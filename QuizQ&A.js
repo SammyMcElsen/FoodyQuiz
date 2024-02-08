22815e54d7adf147a6b895eb120251d588ff31b9
SammyMcElsen <156608126+SammyMcElsen@users.noreply.github.com>
2024-02-08 22:17:26 +0100

    Update QuizQ&A.js
    added answers to options

--- QuizQ&A.js
+++ QuizQ&A.js
@@ -2,201 +2,201 @@ const quizObj = [
     {
     Question: "Which fast food restaurant's logo features four yellow squares on a blue background?",
     Answer: "Greggs", 
-    Options: ["Mc Donalds", "Burger King", "Taco Bell"]
+    Options: ["Greggs","Mc Donalds", "Burger King", "Taco Bell"]
 },
 {
     Question: "What type of pastry are profiteroles made with?",
     Answer: "Choux pastry", 
-    Options: ["Biscuit pastry"]
+    Options: ["Biscuit pastry","Choux pastry"]
 },
 {
     Question: "What is Roquefort cheese made from?",
     Answer: "Ewe's milk", 
-    Options: ["Cow Milk", "Almond Milk", "Goats Milk"]
+    Options: ["Cow milk", "Almond milk","Ewe's milk", "Goats milk"]
 },
 {
     Question: "Are Jaffa Cakes legally a cake or a biscuit?",
     Answer: "A cake", 
-    Options: ["A biscuit"]
+    Options: ["A cake","A biscuit"]
 },
 {
     Question: "What is Eleven's favourite food in Stranger Things?",
     Answer: "Eggos", 
-    Options: ["French Fries", "Ice Cream", "Burger"]
+    Options: ["French Fries", "Eggos", "Ice Cream", "Burger"]
 },
 {
     Question: "What is the most expensive spice in the world by weight?",
     Answer: "Saffron", 
-    Options: ["Curry", "Salt", "Chili"]
+    Options: ["Curry", "Salt", "Saffron", "Chili"]
 },
 {
     Question: "Grenadine comes from which fruit?",
     Answer: "Pomegranate", 
-    Options: ["Apple", "Pear", "Cherry"]
+    Options: ["Pomegranate", "Apple", "Pear", "Cherry"]
 },
 {
     Question: "What kind of dessert did Great British Bake Off star Iain famously throw in the bin back in 2014?",
     Answer: "Baked Alaska", 
-    Options: ["Apple Pie", "Crème Brûlée", "Vanilla Pudding"]
+    Options: ["Apple Pie", "Crème Brûlée", "Baked Alaska", "Vanilla Pudding"]
 },
 {
     Question: "What drink is nicknamed 'the green fairy'?",
     Answer: "Absinthe", 
-    Options: ["Jägermeister", "Midori Melon", "Green Moon Vodka"]
+    Options: ["Jägermeister", "Absinthe", "Midori Melon", "Green Moon Vodka"]
 },
 {
     Question: `Which chocolate's slogan is: "the lighter way to enjoy chocolate"?`,
     Answer: "Maltesers", 
-    Options: ["Milka", "Lindt", "Herseys"]
+    Options: ["Milka", "Lindt", "Maltesers", "Herseys"]
 },
 {
     Question: "Where was Tabasco invented?",
     Answer: "Avery Island, Louisiana", 
-    Options: ["Mexico", "Russia", "Spain"]
+    Options: ["Mexico", "Avery Island, Louisiana", "Russia", "Spain"]
 },
 {
     Question: "What is the main ingredient in sauerkraut?",
     Answer: "Cabbage", 
-    Options: ["Salat", "Spinach", "Cucumbers"]
+    Options: ["Salat", "Spinach", "Cabbage", "Cucumbers"]
 },
 {
     Question: "What is added to a croque monsieur to make it a croque madame?",
     Answer: "An egg", 
-    Options: ["A pear", "An Apple", "Milk" ]
+    Options: ["A pear", "An Apple", "An egg", "Milk" ]
 },
 {
     Question: "Which celebrity released a cookbook called 'From Crook to Cook' in 2018?",
     Answer: "Snoop Dogg", 
-    Options: ["Harry Styles", "Selena Gomez", "Justin Bieber"]
+    Options: ["Snoop Dogg", "Harry Styles", "Selena Gomez", "Justin Bieber"]
 },
 {
     Question: "What are the two main ingredients in Cullen Skink?",
     Answer: "Smoked haddock and potatoes", 
-    Options: ["Salat and bread", "Sausage and potatoes", "Beef and potatoes"]
+    Options: ["Salat and bread", "Sausage and potatoes", "Smoked haddock and potatoes", "Beef and potatoes"]
 },
 {
     Question: "Which of these does not contain nuts?",
     Answer: "Double Decker", 
-    Options: ["Snickers", "Toblerone", "Picnic"]
+    Options: ["Double Decker", "Snickers", "Toblerone", "Picnic"]
 },
 {
     Question: "What is a Goosnargh cake?",
     Answer: "A type of biscuit from Lancashire spiced with carraway seeds", 
-    Options: ["A type of biscuit from Lancashire spiced with sunflower seeds"]
+    Options: ["A type of biscuit from Lancashire spiced with sunflower seeds", "A type of biscuit from Lancashire spiced with carraway seeds", ]
 },
 {
     Question: "Which animal is on the Toblerone logo?",
     Answer: "A bear", 
-    Options: ["A Moose", "An Elk", "A cow"]
+    Options: ["A Moose", "An Elk", "A bear", "A cow"]
 },
 {
     Question: "What is chawanmushi?",
     Answer: "A traditional Japanese breakfast food", 
-    Options: ["A traditional Chinese breakfast food"]
+    Options: ["A traditional Japanese breakfast food", "A traditional Chinese breakfast food"]
 },
 {
     Question: "What is the golden rule of cooking a carbonara?",
     Answer: "Don't add cream", 
-    Options: ["Add cream"]
+    Options: ["Add cream", "Don't add cream"]
 },
 {
     Question: "What is the filling of a Cadbury Creme Egg made of?",
     Answer: "Fondant", 
-    Options: ["Creme", "Egg", "Cheese"]
+    Options: ["Creme", "Fondant", "Egg", "Cheese"]
 },
 {
     Question: "How many spice levels are there at Nando's?",
     Answer: "Seven", 
-    Options: ["Ten", "Five", "Eight"]
+    Options: ["Seven", "Ten", "Five", "Eight"]
 },
 {
     Question: "Who are M&Ms named after?",
     Answer: "They were named after their creators Mars and Murrie", 
-    Options: ["They were named after their mascots Mars and Murrie"]
+    Options: ["They were named after their creators Mars and Murrie", "They were named after their mascots Mars and Murrie"]
 },
 {
     Question: "Where was the Hawaiian pizza invented?",
     Answer: "Ontario, Canada", 
-    Options: ["Hawaii", "Germany", "Los Angeles"]
+    Options: ["Hawaii", "Germany", "Ontario, Canada", "Los Angeles"]
 },
 {
     Question: "What is the American name for coriander?",
     Answer: "Cilantro", 
-    Options: ["Coriander"]
+    Options: ["Cilantro", "Coriander"]
 },
 {
     Question: "A fresh egg will do what in a glass of water?",
     Answer: "Sink", 
-    Options: ["Float"]
+    Options: ["Float", "Sink"]
 },
 {
     Question: "What is the most popular flavour of crisps in the UK?",
     Answer: "Cheese and onion", 
-    Options: ["Paprika", "Chili", "Salt and vinegar"]
+    Options: ["Paprika", "Cheese and onion", "Chili", "Salt and vinegar"]
 },
 {
     Question: "Bucatini, Rigatoni and Strozzapreti are all types of what?",
     Answer: "Pasta", 
-    Options: ["Breat", "Apples", "Potatoes"]
+    Options: ["Breat", "Pasta", "Apples", "Potatoes"]
 },
 {
     Question: "Is an avocado a fruit or vegetable?",
     Answer: "A fruit", 
-    Options: ["A vegetable"]
+    Options: ["A fruit", "A vegetable"]
 },
 {
     Question: "What is the secret to Ross's Thanksgiving sandwich in Friends?",
     Answer: "An extra slice of gravy soaked bread in the middle of the sandwich", 
-    Options: ["An extra slice of beef in the middle of the sandwich"]
+    Options: ["An extra slice of beef in the middle of the sandwich", "An extra slice of gravy soaked bread in the middle of the sandwich", ]
 },
 {
     Question: "What are the typical ingredients of a negroni cocktail?",
     Answer: "Campari, vermouth and gin", 
-    Options: ["Campari, vermouth and vodka"]
+    Options: ["Campari, vermouth and vodka", "Campari, vermouth and gin"]
 },
 {
     Question: "What is alliumphobia a fear of?",
     Answer: "A fear of garlic", 
-    Options: ["A fear of apples", "A fear of sugar", "A fear of calories"]
+    Options: ["A fear of apples", "A fear of sugar", "A fear of garlic", "A fear of calories"]
 },
 {
     Question: "What pastry is Portugal known for?",
     Answer: "Pastel de nata", 
-    Options: ["Pasta de peste"]
+    Options: ["Pasta de peste", "Pastel de nata"]
 },
 {
     Question: "On average how many cups of tea does a Brit drink day?",
     Answer: "2.7", 
-    Options: ["4.7", "5" , "8"]
+    Options: ["4.7", "5" , "8", "2.7"]
 },
 {
     Question: "Who is the 'naked chef'?",
     Answer: "Jamie Oliver", 
-    Options: ["Gordon Ramsay"]
+    Options: ["Jamie Oliver", "Gordon Ramsay"]
 },
 {
     Question: "Which country produces the most coffee?",
     Answer: "Brazil", 
-    Options: ["Spain", "Thailand", "Madagascar"]
+    Options: ["Spain", "Thailand", "Brazil", "Madagascar"]
 },
 {
     Question: "What does 'nasi' mean?",
     Answer: "Rice", 
-    Options: ["Milk", "Sea Food", "Vegetables"]
+    Options: ["Milk", "Sea Food", "Rice", "Vegetables"]
 },
 {
     Question: "How many Prets are there in London?",
     Answer: "267", 
-    Options: ["154", "346", "238"]
+    Options: ["154", "346", "267", "238"]
 },
 {
     Question: "Paella originates from which country?",
-    Answer: "Spain",
-    Options: ["Mexico", "Brazil", "Portugal"]
+    Answer: "Spain", 
+    Options: ["Spain", "Mexico", "Brazil", "Portugal"]
 },
 {
     Question: "A Yorkshire pudding traditionally accompanies what type of roast dinner?",
     Answer: "Beef", 
-    Options: ["Pork", "Chicken", "Turkey"]
+    Options: ["Pork", "Chicken", "Beef", "Turkey"]
 },
 ];
