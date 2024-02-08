790e61d507f2e5bb0230ec623dd7a6fe64c78f0f
SammyMcElsen <156608126+SammyMcElsen@users.noreply.github.com>
2024-02-08 21:05:18 +0100

    added Q&A
    added Q&A as a seperate JS file

---
+++ QuizQ&A.js
@@ -0,0 +1,162 @@
+const quizObj = [
+    {
+    Question: "Which fast food restaurant's logo features four yellow squares on a blue background?",
+    Answer: "Greggs"
+},
+{
+    Question: "What type of pastry are profiteroles made with?",
+    Answer: "Choux pastry"
+},
+{
+    Question: "What is Roquefort cheese made from?",
+    Answer: "Ewe's milk"
+},
+{
+    Question: "Are Jaffa Cakes legally a cake or a biscuit?",
+    Answer: "A cake"
+},
+{
+    Question: "What is Eleven's favourite food in Stranger Things?",
+    Answer: "Eggos"
+},
+{
+    Question: "What is the most expensive spice in the world by weight?",
+    Answer: "Saffron"
+},
+{
+    Question: "Grenadine comes from which fruit?",
+    Answer: "Pomegranate"
+},
+{
+    Question: "What kind of dessert did Great British Bake Off star Iain famously throw in the bin back in 2014?",
+    Answer: "Baked Alaska"
+},
+{
+    Question: "What drink is nicknamed 'the green fairy'?",
+    Answer: "Absinthe"
+},
+{
+    Question: `Which chocolate's slogan is: "the lighter way to enjoy chocolate"?`,
+    Answer: "Maltesers"
+},
+{
+    Question: "Where was Tabasco invented?",
+    Answer: "Avery Island, Louisiana"
+},
+{
+    Question: "What is the main ingredient in sauerkraut?",
+    Answer: "Cabbage"
+},
+{
+    Question: "What is added to a croque monsieur to make it a croque madame?",
+    Answer: "An egg"
+},
+{
+    Question: "Which celebrity released a cookbook called 'From Crook to Cook' in 2018?",
+    Answer: "Snoop Dogg"
+},
+{
+    Question: "What are the two main ingredients in Cullen Skink?",
+    Answer: "Smoked haddock and potatoes"
+},
+{
+    Question: "Which of these does not contain nuts: Snickers, Double Decker, Toblerone, Picnic.",
+    Answer: "Double Decker"
+},
+{
+    Question: "What is a Goosnargh cake?",
+    Answer: "A type of biscuit from Lancashire spiced with carraway seeds"
+},
+{
+    Question: "Which animal is on the Toblerone logo?",
+    Answer: "A bear"
+},
+{
+    Question: "What is chawanmushi?",
+    Answer: "A traditional Japanese breakfast food"
+},
+{
+    Question: "What is the golden rule of cooking a carbonara?",
+    Answer: "Don't add cream"
+},
+{
+    Question: "What is the filling of a Cadbury Creme Egg made of?",
+    Answer: "Fondant"
+},
+{
+    Question: "How many spice levels are there at Nando's?",
+    Answer: "Seven"
+},
+{
+    Question: "Who are M&Ms named after?",
+    Answer: "They were named after their creators Mars and Murrie"
+},
+{
+    Question: "Where was the Hawaiian pizza invented?",
+    Answer: "Ontario, Canada"
+},
+{
+    Question: "What is the American name for coriander?",
+    Answer: "Cilantro"
+},
+{
+    Question: "A fresh egg will do what in a glass of water?",
+    Answer: "Sink"
+},
+{
+    Question: "What is the most popular flavour of crisps in the UK?",
+    Answer: "Cheese and onion"
+},
+{
+    Question: "Bucatini, Rigatoni and Strozzapreti are all types of what?",
+    Answer: "Pasta"
+},
+{
+    Question: "Is an avocado a fruit or vegetable?",
+    Answer: "A fruit"
+},
+{
+    Question: "What is the secret to Ross's Thanksgiving sandwich in Friends?",
+    Answer: "An extra slice of gravy soaked bread in the middle of the sandwich"
+},
+{
+    Question: "What are the typical ingredients of a negroni cocktail?",
+    Answer: "Campari, vermouth and gin"
+},
+{
+    Question: "What is alliumphobia a fear of?",
+    Answer: "A fear of garlic"
+},
+{
+    Question: "What pastry is Portugal known for?",
+    Answer: "Pastel de nata"
+},
+{
+    Question: "On average how many cups of tea does a Brit drink day?",
+    Answer: "2.7"
+},
+{
+    Question: "Who is the 'naked chef'?",
+    Answer: "Jamie Oliver"
+},
+{
+    Question: "Which country produces the most coffee?",
+    Answer: "Brazil"
+},
+{
+    Question: "What does 'nasi' mean?",
+    Answer: "Rice"
+},
+{
+    Question: "How many Prets are there in London?",
+    Answer: "267"
+},
+{
+    Question: "Paella originates from which country?",
+    Answer: "Spain"
+},
+{
+    Question: "A Yorkshire pudding traditionally accompanies what type of roast dinner?",
+    Answer: "Beef"
+},
+];
