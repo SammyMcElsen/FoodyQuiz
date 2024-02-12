91016c9cd3cd6e2abe0d5d48c05576656a4108f7
SammyMcElsen <156608126+SammyMcElsen@users.noreply.github.com>
2024-02-13 00:34:54 +0100

    images added
    added and styled images and healthbar icons

--- script.js
+++ script.js
@@ -3,202 +3,242 @@ const quizData = [
   {
   question: "Which fast food restaurant's logo features four yellow squares on a blue background?",
   answer: "Greggs", 
-  options: ["Greggs","Mc Donalds", "Burger King", "Taco Bell"]
+  options: ["Greggs","Mc Donalds", "Burger King", "Taco Bell"],
+  picture: "pictures/Question1.jpg"
 },
 {
   question: "What type of pastry are profiteroles made with?",
   answer: "Choux pastry", 
-  options: ["Biscuit pastry","Choux pastry"]
+  options: ["Biscuit pastry","Choux pastry"],
+  picture: "pictures/Question2.jpg"
 },
 {
   question: "What is Roquefort cheese made from?",
   answer: "Ewe's milk", 
-  options: ["Cow milk", "Almond milk","Ewe's milk", "Goats milk"]
+  options: ["Cow milk", "Almond milk","Ewe's milk", "Goats milk"],
+  picture: "pictures/Question3.jpg"
 },
 {
   question: "Are Jaffa Cakes legally a cake or a biscuit?",
   answer: "A cake", 
-  options: ["A cake","A biscuit"]
+  options: ["A cake","A biscuit"],
+  picture: "pictures/Question4.jpg"
 },
 {
   question: "What is Eleven's favourite food in Stranger Things?",
   answer: "Eggos", 
-  options: ["French Fries", "Eggos", "Ice Cream", "Burger"]
+  options: ["French Fries", "Eggos", "Ice Cream", "Burger"],
+  picture: "pictures/Question5.jpg"
 },
 {
   question: "What is the most expensive spice in the world by weight?",
   answer: "Saffron", 
-  options: ["Curry", "Salt", "Saffron", "Chili"]
+  options: ["Curry", "Salt", "Saffron", "Chili"],
+  picture: "pictures/Question6.jpg"
 },
 {
   question: "Grenadine comes from which fruit?",
   answer: "Pomegranate", 
-  options: ["Pomegranate", "Apple", "Pear", "Cherry"]
+  options: ["Pomegranate", "Apple", "Pear", "Cherry"],
+  picture: "pictures/Question7.jpg"
 },
 {
   question: "What kind of dessert did Great British Bake Off star Iain famously throw in the bin back in 2014?",
   answer: "Baked Alaska", 
-  options: ["Apple Pie", "Crème Brûlée", "Baked Alaska", "Vanilla Pudding"]
+  options: ["Apple Pie", "Crème Brûlée", "Baked Alaska", "Vanilla Pudding"],
+  picture: "pictures/Question8.jpg"
 },
 {
   question: "What drink is nicknamed 'the green fairy'?",
   answer: "Absinthe", 
-  options: ["Jägermeister", "Absinthe", "Midori Melon", "Green Moon Vodka"]
+  options: ["Jägermeister", "Absinthe", "Midori Melon", "Green Moon Vodka"],
+  picture: "pictures/Question9.jpg"
 },
 {
   question: "Which chocolate's slogan is: 'the lighter way to enjoy chocolate'?",
   answer: "Maltesers", 
-  options: ["Milka", "Lindt", "Maltesers", "Herseys"]
+  options: ["Milka", "Lindt", "Maltesers", "Herseys"],
+  picture: "pictures/Question10.jpg"
 },
 {
   question: "Where was Tabasco invented?",
   answer: "Avery Island, Louisiana", 
-  options: ["Mexico", "Avery Island, Louisiana", "Russia", "Spain"]
+  options: ["Mexico", "Avery Island, Louisiana", "Russia", "Spain"],
+  picture: "pictures/Question11.jpg"
 },
 {
   question: "What is the main ingredient in sauerkraut?",
   answer: "Cabbage", 
-  options: ["Salat", "Spinach", "Cabbage", "Cucumbers"]
+  options: ["Salat", "Spinach", "Cabbage", "Cucumbers"],
+  picture: "pictures/Question12.jpg"
 },
 {
   question: "What is added to a croque monsieur to make it a croque madame?",
   answer: "An egg", 
-  options: ["A pear", "An Apple", "An egg", "Milk" ]
+  options: ["A pear", "An Apple", "An egg", "Milk" ],
+  picture: "pictures/Question13.jpg"
 },
 {
   question: "Which celebrity released a cookbook called 'From Crook to Cook' in 2018?",
   answer: "Snoop Dogg", 
-  options: ["Snoop Dogg", "Harry Styles", "Selena Gomez", "Justin Bieber"]
+  options: ["Snoop Dogg", "Harry Styles", "Selena Gomez", "Justin Bieber"],
+  picture: "pictures/Question14.jpg"
 },
 {
   question: "What are the two main ingredients in Cullen Skink?",
   answer: "Smoked haddock and potatoes", 
-  options: ["Salat and bread", "Sausage and potatoes", "Smoked haddock and potatoes", "Beef and potatoes"]
+  options: ["Salat and bread", "Sausage and potatoes", "Smoked haddock and potatoes", "Beef and potatoes"],
+  picture: "pictures/Question15.webp"
 },
 {
   question: "Which of these does not contain nuts?",
   answer: "Double Decker", 
-  options: ["Double Decker", "Snickers", "Toblerone", "Picnic"]
+  options: ["Double Decker", "Snickers", "Toblerone", "Picnic"],
+  picture: "pictures/Question16.jpg"
 },
 {
   question: "What is a Goosnargh cake?",
   answer: "A type of biscuit from Lancashire spiced with carraway seeds", 
-  options: ["A type of biscuit from Lancashire spiced with sunflower seeds", "A type of biscuit from Lancashire spiced with carraway seeds"]
+  options: ["A type of biscuit from Lancashire spiced with sunflower seeds", "A type of biscuit from Lancashire spiced with carraway seeds"],
+  picture: "pictures/Question17.jpg"
 },
 {
   question: "Which animal is on the Toblerone logo?",
   answer: "A bear", 
-  options: ["A Moose", "An Elk", "A bear", "A cow"]
+  options: ["A Moose", "An Elk", "A bear", "A cow"],
+  picture: "pictures/Question18.jpg"
 },
 {
   question: "What is chawanmushi?",
   answer: "A traditional Japanese breakfast food", 
-  options: ["A traditional Japanese breakfast food", "A traditional Chinese breakfast food"]
+  options: ["A traditional Japanese breakfast food", "A traditional Chinese breakfast food"],
+  picture: "pictures/Question19.jpg"
 },
 {
   question: "What is the golden rule of cooking a carbonara?",
   answer: "Don't add cream", 
-  options: ["Add cream", "Don't add cream"]
+  options: ["Add cream", "Don't add cream"],
+  picture: "pictures/Question20.jpg"
 },
 {
   question: "What is the filling of a Cadbury Creme Egg made of?",
   answer: "Fondant", 
-  options: ["Creme", "Fondant", "Egg", "Cheese"]
+  options: ["Creme", "Fondant", "Egg", "Cheese"],
+  picture: "pictures/Question21.jpg"
 },
 {
   question: "How many spice levels are there at Nando's?",
   answer: "Seven", 
-  options: ["Seven", "Ten", "Five", "Eight"]
+  options: ["Seven", "Ten", "Five", "Eight"],
+  picture: "pictures/Question22.jpg"
 },
 {
   question: "Who are M&Ms named after?",
   answer: "They were named after their creators Mars and Murrie", 
-  options: ["They were named after their creators Mars and Murrie", "They were named after their mascots Mars and Murrie"]
+  options: ["They were named after their creators Mars and Murrie", "They were named after their mascots Mars and Murrie"],
+  picture: "pictures/Question23.jpg"
 },
 {
   question: "Where was the Hawaiian pizza invented?",
   answer: "Ontario, Canada", 
-  options: ["Hawaii", "Germany", "Ontario, Canada", "Los Angeles"]
+  options: ["Hawaii", "Germany", "Ontario, Canada", "Los Angeles"],
+  picture: "pictures/Question24.jpg"
 },
 {
   question: "What is the American name for coriander?",
   answer: "Cilantro", 
-  options: ["Cilantro", "Coriander"]
+  options: ["Cilantro", "Coriander"],
+  picture: "pictures/Question25.jpg"
 },
 {
   question: "A fresh egg will do what in a glass of water?",
   answer: "Sink", 
-  options: ["Float", "Sink"]
+  options: ["Float", "Sink"],
+  picture: "pictures/Question26.jpg"
 },
 {
   question: "What is the most popular flavour of crisps in the UK?",
   answer: "Cheese and onion", 
-  options: ["Paprika", "Cheese and onion", "Chili", "Salt and vinegar"]
+  options: ["Paprika", "Cheese and onion", "Chili", "Salt and vinegar"],
+  picture: "pictures/Question27.jpg"
 },
 {
   question: "Bucatini, Rigatoni and Strozzapreti are all types of what?",
   answer: "Pasta", 
-  options: ["Breat", "Pasta", "Apples", "Potatoes"]
+  options: ["Breat", "Pasta", "Apples", "Potatoes"],
+  picture: "pictures/Question28.jpg"
 },
 {
   question: "Is an avocado a fruit or vegetable?",
   answer: "A fruit", 
-  options: ["A fruit", "A vegetable"]
+  options: ["A fruit", "A vegetable"],
+  picture: "pictures/Question29.jpg"
 },
 {
   question: "What is the secret to Ross's Thanksgiving sandwich in Friends?",
   answer: "An extra slice of gravy soaked bread in the middle of the sandwich", 
-  options: ["An extra slice of beef in the middle of the sandwich", "An extra slice of gravy soaked bread in the middle of the sandwich"]
+  options: ["An extra slice of beef in the middle of the sandwich", "An extra slice of gravy soaked bread in the middle of the sandwich"],
+  picture: "pictures/Question30.jpg"
 },
 {
   question: "What are the typical ingredients of a negroni cocktail?",
   answer: "Campari, vermouth and gin", 
-  options: ["Campari, vermouth and vodka", "Campari, vermouth and gin"]
+  options: ["Campari, vermouth and vodka", "Campari, vermouth and gin"],
+  picture: "pictures/Question31.jpg"
 },
 {
   question: "What is alliumphobia a fear of?",
   answer: "A fear of garlic", 
-  options: ["A fear of apples", "A fear of sugar", "A fear of garlic", "A fear of calories"]
+  options: ["A fear of apples", "A fear of sugar", "A fear of garlic", "A fear of calories"],
+  picture: "pictures/Question32.jpg"
 },
 {
   question: "What pastry is Portugal known for?",
   answer: "Pastel de nata", 
-  options: ["Pasta de peste", "Pastel de nata"]
+  options: ["Pasta de peste", "Pastel de nata"],
+  picture: "pictures/Question33.jpg"
 },
 {
   question: "On average how many cups of tea does a Brit drink day?",
   answer: "2.7", 
-  options: ["4.7", "5" , "8", "2.7"]
+  options: ["4.7", "5" , "8", "2.7"],
+  picture: "pictures/Question34.jpg"
 },
 {
   question: "Who is the 'naked chef'?",
   answer: "Jamie Oliver", 
-  options: ["Jamie Oliver", "Gordon Ramsay"]
+  options: ["Jamie Oliver", "Gordon Ramsay"],
+  picture: "pictures/Question35.jpg"
 },
 {
   question: "Which country produces the most coffee?",
   answer: "Brazil", 
-  options: ["Spain", "Thailand", "Brazil", "Madagascar"]
+  options: ["Spain", "Thailand", "Brazil", "Madagascar"],
+  picture: "pictures/Question36.jpg"
 },
 {
   question: "What does 'nasi' mean?",
   answer: "Rice", 
-  options: ["Milk", "Sea Food", "Rice", "Vegetables"]
+  options: ["Milk", "Sea Food", "Rice", "Vegetables"],
+  picture: "pictures/Question37.jpg"
 },
 {
   question: "How many Prets are there in London?",
   answer: "267", 
-  options: ["154", "346", "267", "238"]
+  options: ["154", "346", "267", "238"],
+  picture: "pictures/Question38.jpeg"
 },
 {
   question: "Paella originates from which country?",
   answer: "Spain", 
-  options: ["Spain", "Mexico", "Brazil", "Portugal"]
+  options: ["Spain", "Mexico", "Brazil", "Portugal"],
+  picture: "pictures/Question39.jpg"
 },
 {
   question: "A Yorkshire pudding traditionally accompanies what type of roast dinner?",
   answer: "Beef", 
-  options: ["Pork", "Chicken", "Beef", "Turkey"]
+  options: ["Pork", "Chicken", "Beef", "Turkey"],
+  picture: "pictures/Question40.jpg"
 }
 ];
 
@@ -207,10 +247,13 @@ const resultContainer = document.getElementById('result');
 const submitButton = document.getElementById('submit');
 const retryButton = document.getElementById('retry');
 const showAnswerButton = document.getElementById('showAnswer');
+const imageContainer = document.getElementById('image');
+const healthContainer = document.getElementById('health');
 
 let currentQuestion = 0;
 let score = 0;
 let health = 10;
+let lostHealth = 0;
 let incorrectAnswers = [];
 
 function shuffleArray(array) {
@@ -242,6 +285,11 @@ function displayQuestion() {
   questionHeader.appendChild(questionNumberElement);
   questionHeader.appendChild(scoreElement);
 
+  const imageElement = document.createElement('img');
+  imageElement.setAttribute('src',questionData.picture)
+
+
+  healthContainer.className = 'healthbar';
 
 
   const shuffledOptions = [...questionData.options];
@@ -267,6 +315,25 @@ function displayQuestion() {
   quizContainer.appendChild(questionHeader);
   quizContainer.appendChild(questionElement);
   quizContainer.appendChild(optionsElement);
+
+  imageContainer.innerHTML = '';
+  imageContainer.appendChild(imageElement);
+  
+  healthContainer.innerHTML = '';
+  const heart = document.createElement('img');
+  heart.setAttribute('src', "pictures/healthIcon.png");
+
+  const negativeHeart = document.createElement('img');
+  negativeHeart.className = 'lostHeart';
+  negativeHeart.setAttribute('src', "pictures/healthIcon.png");
+
+  for (let i = 0; i < health; i++) {
+    healthContainer.appendChild(heart.cloneNode(true));
+  }
+
+  for (let i = 0; i < lostHealth; i++) {
+    healthContainer.appendChild(negativeHeart.cloneNode(true));
+  }
 }
 
 function checkAnswer() {
@@ -277,6 +344,7 @@ function checkAnswer() {
       score++;
     } else {
       health = health - 1;
+      lostHealth = lostHealth + 1;
       incorrectAnswers.push({
         question: quizData[currentQuestion].question,
         incorrectAnswer: answer,
@@ -314,6 +382,7 @@ function retryQuiz() {
   currentQuestion = 0;
   score = 0;
   health = 10;
+  lostHealth = 0;
   incorrectAnswers = [];
   quizContainer.style.display = 'block';
   submitButton.style.display = 'inline-block';
